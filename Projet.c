550a512ed4a5db010de9f5949e25cd7c5a1f4386
Fazil Ait Yakoub <fazil.aityakoub@yahoo.com>
2023-10-10 17:41:05 +0200

    Première étape fichier créé

--- Projet.c
+++ Projet.c
@@ -1,7 +1,43 @@
 //PROJET C
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    printf("Projet de C !");
+#define NMAX 100
+
+char * SaisieChaine(){
+    char *s;
+    char buffer[NMAX];
+    printf("Entrez une IP : ",NMAX-1);
+    fflush(stdout);
+    fgets(buffer,NMAX-1,stdin);
+    s = (char *)malloc((strlen(buffer)+1)*sizeof(char));
+    if (!s){
+        printf("Problème d'alloc mémoire");
+        exit(0);
+    }
+    strcpy(s,buffer);
+    return s;
+}
+
+int main(int argc, char *argv[]){
+    FILE* fic = NULL;
+    char *IP;
+
+    fic = fopen("test.txt", "w");
+ 
+    if (fic != NULL)
+    {
+
+        IP = SaisieChaine();
+
+        fprintf(fic,IP);
+        fclose(fic);
+
+        fgets(IP,NMAX,fic);
+        printf("%s",IP);
+    }
+ 
+    return 0;
 }
