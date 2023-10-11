c92296c0d7e79bacc8c7e7fd48b543a6c337b9cd
Fazil Ait Yakoub <fazil.aityakoub@yahoo.com>
2023-10-11 18:06:17 +0200

    Ajout d'une fonction qui détecte si une adresse IP est valide ou pas

--- Projet.c
+++ Projet.c
@@ -3,10 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define NMAX 100
 
-char * SaisieChaine(){
+
+char * SaisieIP(){
     char *s;
     char buffer[NMAX];
     printf("Entrez une IP : ",NMAX-1);
@@ -21,23 +23,47 @@ char * SaisieChaine(){
     return s;
 }
 
-int main(int argc, char *argv[]){
+bool IPValide(const char *ip) {
+    int segments[4];
+    int numSegments = sscanf(ip, "%d.%d.%d.%d", &segments[0], &segments[1], &segments[2], &segments[3]);
+
+    if (numSegments != 4) {
+        return false;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        if (segments[i] < 0 || segments[i] > 255) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
     FILE* fic = NULL;
     char *IP;
 
     fic = fopen("test.txt", "w");
  
-    if (fic != NULL)
-    {
+    if (fic != NULL) {
 
-        IP = SaisieChaine();
+        IP = SaisieIP();
+        
+        if (IPValide(IP)) {
+            fprintf(fic, "%s", IP);
+            fclose(fic);
 
-        fprintf(fic,IP);
-        fclose(fic);
-
-        fgets(IP,NMAX,fic);
-        printf("%s",IP);
+            printf("Voici l'IP saisie : %s\n", IP);
+        } else {
+            printf("L'adresse IP saisie n'est pas valide.\n");
+        }
+    } else {
+        printf("Impossible d'ouvrir le fichier.\n");
     }
  
     return 0;
 }
+
+
+
